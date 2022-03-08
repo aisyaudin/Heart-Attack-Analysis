87fdef57c803d5ca80dbf1489b7f9c26128e55e2
aisyaudin98 <100677928+aisyaudin98@users.noreply.github.com>
2022-03-08 17:53:38 +0800

    Add files via upload

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,48 +1,105 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
+from PIL import Image
 
-st.write("""
-# Simple Iris Flower Prediction App
-This app predicts the **Iris flower** type!
-""")
+option = st.sidebar.selectbox(
+  'Select one:', ['Heart Attack Dataset','Analysis'])
 
-st.sidebar.header('User Input Parameters')
+if option=='Heart Attack Dataset':
+  st.header('Heart Attack Dataset')
+  st.markdown("""
+  A heart attack, also called a myocardial infarction, happens when a part of the heart muscle doesn't get enough blood. Coronary artery disease (CAD) is the main cause of heart attack.
+  This project will present the heart attack analytics which provide the possibility rate of the presence of heart disease in the patient.
+  * **Data Source:** [https://www.kaggle.com/nareshbhat/health-care-data-set-on-heart-attack-possibility)
+  """ )
 
-def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-    data = {'sepal_length': sepal_length,
-            'sepal_width': sepal_width,
-            'petal_length': petal_length,
-            'petal_width': petal_width}
-    features = pd.DataFrame(data, index=[0])
-    return features
+  img = Image.open("heart-attack.jpg")
+  st.image(img, use_column_width = True)
 
-df = user_input_features()
+  st.write("- The Heart Analysis Dataset:")
+    
+  path ='heart.csv'
+  df = pd.read_csv('heart.csv')
+  df.astype({"sex": object})
+  df.astype({"fbs": object})
+  df.astype({"restecg": object})
+  df.astype({"exang": object})
+  row_indexes=df[df['age'] >= 40].index
+  df.loc[row_indexes,'elderly']="yes"
+  row_indexes=df[df['age'] < 40].index
+  df.loc[row_indexes,'elderly']="no"
+  df.head(10)
+  st.dataframe(df)
 
-st.subheader('User Input parameters')
-st.write(df)
+  obj = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal']
+  for column in obj:
+    st.write(column, ':', df[column].unique(), '\n')
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
+elif option=='Analysis':
+  st.write("The analysis of heart attack using different machine learning")
+  path ='heart.csv'
+  df = pd.read_csv('heart.csv')
+  df.astype({"sex": object})
+  df.astype({"fbs": object})
+  df.astype({"restecg": object})
+  df.astype({"exang": object})
+  row_indexes=df[df['age'] >= 40].index
+  df.loc[row_indexes,'elderly']="yes"
+  row_indexes=df[df['age'] < 40].index
+  df.loc[row_indexes,'elderly']="no"
+  feature_cols = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal']
+  X = df[feature_cols] 
+  y = df.target
+  from sklearn.model_selection import train_test_split
+  from sklearn.model_selection import train_test_split
+  Xtrain, Xtest, ytrain, ytest = train_test_split(X, y)
+  Xtrain.head()
+  ytrain.head()
+  models = pd.DataFrame(columns=["Model","Accuracy Score"])
 
-clf = RandomForestClassifier()
-clf.fit(X, Y)
+  # GROUP BY ELDERLY AND SEX 
+  from sklearn.preprocessing import LabelEncoder
+  labelencoder = LabelEncoder()
+  df['elderly'] = labelencoder.fit_transform(df['elderly'])
+  df.groupby(['elderly','sex']).mean() 
+    
+  # LOGISTIC REGRESSION
+  from sklearn.linear_model import LogisticRegression
+  from sklearn.metrics import accuracy_score
+  logreg = LogisticRegression()
+  logreg.fit(Xtrain, ytrain)
+  ypred = logreg.predict(Xtest)
+  score1 = accuracy_score(ytest, ypred)
+  st.write("Logistic Regression is equal to", score1)
 
-prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
+  # KNN CLASSIFIER 
+  from sklearn.neighbors import KNeighborsClassifier
+  knn = KNeighborsClassifier()
+  knn.fit(Xtrain, ytrain)
+  ypred = knn.predict(Xtest)
+  score2 = accuracy_score(ytest, ypred)
+  st.write("KNeighborsClassifier is equal to", score2)
 
-st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
+  # SVM
+  from sklearn.svm import SVC
+  svc = SVC()
+  svc.fit(Xtrain, ytrain)
+  ypred = svc.predict(Xtest)
+  score3 = accuracy_score(ytest, ypred)
+  st.write("SVM is equal to", score3)
 
-st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-# st.write(prediction)
+  # RANDOM FOREST
+  from sklearn.ensemble import RandomForestClassifier
+  RandomForest = RandomForestClassifier()
+  RandomForest.fit(Xtrain, ytrain)
+  ypred = RandomForest.predict(Xtest)
+  score4 = accuracy_score(ytest, ypred)
+  st.write("Random Forest Classifier is equal to", score4)
 
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+  st.write("- From all the scores, the accuracy of all the outputs from the model are between 63 percent to 82 percent.")
+
+  st.header('Conclusion')
+  st.write("- Tips to keep your heart healthy.You can prevent heart disease by making these lifestyle choices.")
+  img2 = Image.open("caring-heart-healthy-tips.jpg")
+  st.image(img2, use_column_width = True)
